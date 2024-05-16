c136f6f3fdd0caa582c83b3facf458577d4bcfb5
NicolasFrouin <frouinnicolas@gmaiL.com>
2024-05-16 10:40:06 +0200

    init sequelize

---
+++ config/config.js
@@ -0,0 +1,9 @@
+exports.PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\s\w&])[^\s&]{12,63}$/;
+
+exports.EMAIL_REGEX = /^[-+\w.]+@([\w-]+\.)+[\w-]{1,16}$/;
+
+exports.ROLES = {
+  user: "ROLE_USER",
+  accountant: "ROLE_ACCOUNTANT",
+  admin: "ROLE_ADMIN",
+};
