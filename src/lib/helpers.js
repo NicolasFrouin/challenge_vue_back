6d84fc5ce5a26853fdd0ae975ebb1fe8bdd98ac7
NicolasFrouin <frouinnicolas@gmaiL.com>
2024-05-16 16:01:50 +0200

    add doc for checkProps & rename 'req' param to 'object'

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -2,11 +2,21 @@ const _ = require("lodash");
 const Return = require("./return");
 
 class Helpers {
-  static checkProps(req, paramsToCheck = []) {
+  /**
+   * Checks if the given object contains all the specified properties
+   *
+   * For every missing property, the Return object will have it added to its `errors` array
+   *
+   * @param {object} object The object to check
+   * @param {string[]} paramsToCheck An array of property names to check for - to check nested properties, use dot notation (e.g. "user.name")
+   *
+   * @returns {Return} An instance of the Return class containing the result of the check
+   */
+  static checkProps(object, paramsToCheck = []) {
     const result = new Return(200, {});
     paramsToCheck.forEach((param) => {
-      if (_.has(req, param)) {
-        result.data[param] = req[param];
+      if (_.has(object, param)) {
+        result.data[param] = object[param];
       } else {
         result.code = 400;
         result.setError({ message: `Missing parameter : ${param}`, code: 400 });
